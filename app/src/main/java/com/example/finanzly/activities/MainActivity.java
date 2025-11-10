73f70f7a3cd38a85c7c8c030db318c2c790eb1b4
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2025-11-10 20:20:16 +0100

    Creacion de los fragment y la funcionalidad de cambiar entre ellos

--- app/src/main/java/com/example/finanzly/activities/MainActivity.java
+++ app/src/main/java/com/example/finanzly/activities/MainActivity.java
@@ -2,25 +2,59 @@ package com.example.finanzly.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.finanzly.R;
+import com.example.finanzly.fragments.BudgetsFragment;
+import com.example.finanzly.fragments.GoalsFragment;
+import com.example.finanzly.fragments.MovementsFragment;
+import com.example.finanzly.fragments.RemindersFragment;
+import com.example.finanzly.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_movements) {
+                selectedFragment = new MovementsFragment();
+            } else if (id == R.id.nav_budgets) {
+                selectedFragment = new BudgetsFragment();
+            } else if (id == R.id.nav_goals) {
+                selectedFragment = new GoalsFragment();
+            } else if (id == R.id.nav_reminders) {
+                selectedFragment = new RemindersFragment();
+            } else if (id == R.id.nav_profile) {
+                selectedFragment = new UserFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
         });
+
+
+        // Cargar fragmento inicial
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new MovementsFragment())
+                    .commit();
+        }
     }
 }

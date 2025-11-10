1c71a529d85fc160092687800e1e8ff56638b9e6
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2025-11-10 17:37:00 +0100

    Creacion de proyecto y correccion de errores del gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
 
-    id("com.android.application")
-
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -29,6 +26,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,7 +34,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -44,7 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
 
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
 }
